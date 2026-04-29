00e54a2aac3d525b0f621cd85ca025c4d4d20bc3
Artyom <zujkov.ks52@gmail.com>
2026-04-29 03:14:40 +0300

    добавлен сервис реги юзеров для тестов

---
+++ Infrastructure/Services/InMemoryUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Services
+{
+    public class InMemoryUserRepository : IUserRepository
+    {
+        private readonly List<User> _users = new();
+
+        public Task<User?> GetByPhoneAsync(string phoneNumber, CancellationToken ct)
+        {
+            var user 
+        }
+    }
+}
