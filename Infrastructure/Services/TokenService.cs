415a3c7410d58523a77926a8e26c5157ccaf9df5
Artyom <zujkov.ks52@gmail.com>
2026-06-11 02:27:13 +0300

    JWT авторизация

---
+++ Infrastructure/Services/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Infrastructure.Configurations;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Infrastructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtOptions _jwtOptions;
+        public TokenService(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public string GenerateAccessToken(Guid userId)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()) 
+            };
+
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpireMinutes),
+                signingCredentials: credentials
+            );
+
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
